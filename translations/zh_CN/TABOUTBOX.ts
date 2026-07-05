49c0b079d665e3612ce158122e8ac314899c72da
Yaosong Li <liyaosong1@qq.com>
2026-07-05 18:24:07 +0800

    Complete zh_CN About Box translations

--- translations/zh_CN/TABOUTBOX.ts
+++ translations/zh_CN/TABOUTBOX.ts
@@ -1,73 +1,72 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-
-<TS version="2.1" language="zh_CN" sourcelanguage="en">
-  <context>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
     <name>TABOUTBOX</name>
     <message>
-      <source>&amp;Close</source>
-      <comment>Caption (max 6 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>&amp;Close</source>
+        <comment>Caption (max 6 bytes)</comment>
+        <translation>&amp;关闭</translation>
     </message>
     <message>
-      <source>About MEGA</source>
-      <comment>Caption (max 10 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>About MEGA</source>
+        <comment>Caption (max 10 bytes)</comment>
+        <translation>关于MEGA</translation>
     </message>
     <message>
-      <source>Authors:</source>
-      <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Authors:</source>
+        <comment>Caption (max 8 bytes)</comment>
+        <translation>作者:</translation>
     </message>
     <message>
-      <source>Copyright 2017</source>
-      <comment>Caption (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Copyright 2017</source>
+        <comment>Caption (max 14 bytes)</comment>
+        <translation>Copyright 2017</translation>
     </message>
     <message>
-      <source>DEBUG VERSION</source>
-      <comment>Caption (max 13 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>DEBUG VERSION</source>
+        <comment>Caption (max 13 bytes)</comment>
+        <translation>调试版本</translation>
     </message>
     <message>
-      <source>Glen Stecher</source>
-      <comment>Caption (max 12 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Glen Stecher</source>
+        <comment>Caption (max 12 bytes)</comment>
+        <translation>Glen Stecher</translation>
     </message>
     <message>
-      <source>Koichiro Tamura</source>
-      <comment>Caption (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Koichiro Tamura</source>
+        <comment>Caption (max 15 bytes)</comment>
+        <translation>Koichiro Tamura</translation>
     </message>
     <message>
-      <source>Max Sanderford</source>
-      <comment>Caption (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Max Sanderford</source>
+        <comment>Caption (max 14 bytes)</comment>
+        <translation>Max Sanderford</translation>
     </message>
     <message>
-      <source>Mike Suleski</source>
-      <comment>Caption (max 12 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Mike Suleski</source>
+        <comment>Caption (max 12 bytes)</comment>
+        <translation>Mike Suleski</translation>
     </message>
     <message>
-      <source>Qiqing Tao</source>
-      <comment>Caption (max 10 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Qiqing Tao</source>
+        <comment>Caption (max 10 bytes)</comment>
+        <translation>Qiqing Tao</translation>
     </message>
     <message>
-      <source>Sudhir Kumar</source>
-      <comment>Caption (max 12 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Sudhir Kumar</source>
+        <comment>Caption (max 12 bytes)</comment>
+        <translation>Sudhir Kumar</translation>
     </message>
     <message>
-      <source>Sudip Sharma</source>
-      <comment>Caption (max 12 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Sudip Sharma</source>
+        <comment>Caption (max 12 bytes)</comment>
+        <translation>Sudip Sharma</translation>
     </message>
     <message>
-      <source>Version 7</source>
-      <comment>Caption (max 9 bytes)</comment>
-      <translation type="unfinished"/>
+        <source>Version 7</source>
+        <comment>Caption (max 9 bytes)</comment>
+        <translation>版本 7</translation>
     </message>
-  </context>
+</context>
 </TS>
