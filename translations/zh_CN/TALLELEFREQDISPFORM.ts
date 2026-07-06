baeee8cf7684780f11641ba792205b15fdf34ddd
Yaosong Li <liyaosong1@qq.com>
2026-07-06 14:04:09 +0800

    Update TALLELEFREQDISPFORM.ts

--- translations/zh_CN/TALLELEFREQDISPFORM.ts
+++ translations/zh_CN/TALLELEFREQDISPFORM.ts
@@ -7,22 +7,22 @@
     <message>
       <source>&amp;Analysis</source>
       <comment>Caption (max 9 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">分析(&amp;A)</translation>
     </message>
     <message>
       <source>&amp;Data</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">数据(&amp;D)</translation>
     </message>
     <message>
       <source>&amp;Display</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">显示(&amp;D)</translation>
     </message>
     <message>
       <source>&amp;Export...</source>
       <comment>Caption (max 10 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">导出(&amp;E)...</translation>
     </message>
     <message>
       <source>AlleleFreqDispForm</source>
@@ -32,47 +32,47 @@
     <message>
       <source>Analyze Individual Loci &amp;Within Populations</source>
       <comment>Caption (max 43 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">分析群体内各位点</translation>
     </message>
     <message>
       <source>Analyze Individual Loci in a Subdivided populations</source>
       <comment>Caption (max 51 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">分析亚分化群体的各位点</translation>
     </message>
     <message>
       <source>Analyze Multiple Loci in a Subdivided Population</source>
       <comment>Caption (max 48 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">分析亚分化群体的多位点</translation>
     </message>
     <message>
       <source>Change &amp;Font</source>
       <comment>Caption (max 12 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">更改字体(&amp;F)</translation>
     </message>
     <message>
       <source>Display allele frequencies in the data grid</source>
       <comment>Hint (max 43 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">在数据网格中显示等位基因频率</translation>
     </message>
     <message>
       <source>Export data to a text file</source>
       <comment>Hint (max 26 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">将数据导出为文本文件</translation>
     </message>
     <message>
       <source>Frequencies</source>
       <comment>Caption (max 11 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">频率</translation>
     </message>
     <message>
       <source>Input Data File Name</source>
       <comment>Text (max 20 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">输入数据文件名</translation>
     </message>
     <message>
       <source>P-values</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">P 值</translation>
     </message>
     <message>
       <source>QuitViewerAction</source>
@@ -87,7 +87,7 @@
     <message>
       <source>Show Only Selected &amp;Populations</source>
       <comment>Caption (max 31 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">仅显示选中的群体(&amp;P)</translation>
     </message>
     <message>
       <source>ShowExpGenotypeFreqAction</source>
@@ -102,7 +102,7 @@
     <message>
       <source>Toggle the display between allele frequencies and p-values</source>
       <comment>Hint (max 58 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">在等位基因频率与 P 值之间切换显示</translation>
     </message>
     <message>
       <source>ToolBar1</source>
