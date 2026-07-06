f780f1f1379170c4d1676e8e4c152b179990dd81
Yaosong Li <liyaosong1@qq.com>
2026-07-06 14:00:49 +0800

    Update TALIGNMENTDATATYPEDLG.ts

--- translations/zh_CN/TALIGNMENTDATATYPEDLG.ts
+++ translations/zh_CN/TALIGNMENTDATATYPEDLG.ts
@@ -7,27 +7,27 @@
     <message>
       <source>&amp;Cancel</source>
       <comment>Caption (max 7 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">取消(&amp;C)</translation>
     </message>
     <message>
       <source>&amp;DNA</source>
       <comment>Caption (max 4 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">DNA(&amp;D)</translation>
     </message>
     <message>
       <source>&amp;Protein</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">蛋白质(&amp;P)</translation>
     </message>
     <message>
       <source>Are you building a DNA or protein sequence alignment?</source>
       <comment>Caption (max 53 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">您正在构建 DNA 还是蛋白质序列比对？</translation>
     </message>
     <message>
       <source>Data Type for Alignment</source>
       <comment>Caption (max 23 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">比对数据类型</translation>
     </message>
   </context>
 </TS>
