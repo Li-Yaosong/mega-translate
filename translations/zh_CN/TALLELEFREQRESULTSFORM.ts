f69889e529b0da9d137c5c12098dc99ce569927d
Yaosong Li <liyaosong1@qq.com>
2026-07-06 16:18:54 +0800

    Update TALLELEFREQRESULTSFORM.ts

--- translations/zh_CN/TALLELEFREQRESULTSFORM.ts
+++ translations/zh_CN/TALLELEFREQRESULTSFORM.ts
@@ -7,57 +7,57 @@
     <message>
       <source>&amp;Save</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">保存(&amp;S)</translation>
     </message>
     <message>
       <source>AlleleFreqResultsForm</source>
       <comment>Caption (max 21 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">AlleleFreqResultsForm</translation>
     </message>
     <message>
       <source>CloseAction</source>
       <comment>Caption (max 11 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">CloseAction</translation>
     </message>
     <message>
       <source>Copy results to the system clipboard</source>
       <comment>Hint (max 36 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">复制结果到系统剪贴板</translation>
     </message>
     <message>
       <source>CopyAction</source>
       <comment>Caption (max 10 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">CopyAction</translation>
     </message>
     <message>
       <source>PrintAction</source>
       <comment>Caption (max 11 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">PrintAction</translation>
     </message>
     <message>
       <source>Save Results As</source>
       <comment>Title (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">结果另存为</translation>
     </message>
     <message>
       <source>Save the results to a text file</source>
       <comment>Hint (max 31 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">将结果保存到文本文件</translation>
     </message>
     <message>
       <source>Send the results to a network printer</source>
       <comment>Hint (max 37 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">将结果发送到网络打印机</translation>
     </message>
     <message>
       <source>ToolBar1</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">ToolBar1</translation>
     </message>
     <message>
       <source>ToolBar2</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">ToolBar2</translation>
     </message>
   </context>
 </TS>
