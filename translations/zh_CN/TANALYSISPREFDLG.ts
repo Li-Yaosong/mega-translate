096de303543b031e7a9c605a551ed5a19b8f5457
Yaosong Li <liyaosong1@qq.com>
2026-07-10 10:08:33 +0800

    Update TANALYSISPREFDLG.ts

--- translations/zh_CN/TANALYSISPREFDLG.ts
+++ translations/zh_CN/TANALYSISPREFDLG.ts
@@ -7,167 +7,167 @@
     <message>
       <source>&amp;Cancel</source>
       <comment>Caption (max 7 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">取消(&amp;C)</translation>
     </message>
     <message>
       <source>&amp;Help</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">帮助(&amp;H)</translation>
     </message>
     <message>
       <source>&amp;Ok</source>
       <comment>Caption (max 3 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">确定(&amp;O)</translation>
     </message>
     <message>
       <source>&amp;Save Settings</source>
       <comment>Caption (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">保存设置(&amp;S)</translation>
     </message>
     <message>
       <source>-</source>
       <comment>Text (max 1 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">-</translation>
     </message>
     <message>
       <source>.</source>
       <comment>Text (max 1 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">.</translation>
     </message>
     <message>
       <source>1st</source>
       <comment>Caption (max 3 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">第1</translation>
     </message>
     <message>
       <source>2nd</source>
       <comment>Caption (max 3 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">第2</translation>
     </message>
     <message>
       <source>3rd</source>
       <comment>Caption (max 3 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">第3</translation>
     </message>
     <message>
       <source>?</source>
       <comment>Text (max 1 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">?</translation>
     </message>
     <message>
       <source>Abort the current action</source>
       <comment>Hint (max 24 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">中止当前操作</translation>
     </message>
     <message>
       <source>Adaptive</source>
       <comment>Text (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">自适应</translation>
     </message>
     <message>
       <source>All Sites</source>
       <comment>Text (max 9 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">所有位点</translation>
     </message>
     <message>
       <source>AnalysisPrefDlg</source>
       <comment>Caption (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">分析首选项</translation>
     </message>
     <message>
       <source>AppOptionsSheet</source>
       <comment>Caption (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">应用选项</translation>
     </message>
     <message>
       <source>Data Type:</source>
       <comment>Caption (max 10 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">数据类型：</translation>
     </message>
     <message>
       <source>Gaps/Identical/Missing Data Treatment</source>
       <comment>Caption (max 37 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">空位/相同/缺失数据处理</translation>
     </message>
     <message>
       <source>Give up after</source>
       <comment>Caption (max 13 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">超时放弃（小时）</translation>
     </message>
     <message>
       <source>Labels to Include</source>
       <comment>Caption (max 17 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">要包含的标签</translation>
     </message>
     <message>
       <source>Noncoding Sites</source>
       <comment>Caption (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">非编码位点</translation>
     </message>
     <message>
       <source>Nucleotide (non-coding)</source>
       <comment>Caption (max 23 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">核苷酸（非编码）</translation>
     </message>
     <message>
       <source>Open the help manual</source>
       <comment>Hint (max 20 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">打开帮助手册</translation>
     </message>
     <message>
       <source>Options Summary</source>
       <comment>Caption (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">选项摘要</translation>
     </message>
     <message>
       <source>Please note that if missing, identical, or gap symbol options are specified in the 'Format' command of a .meg file, those options will override the options specified here, which are saved in the MEGA Analysis Options (.mao) file.</source>
       <comment>Caption (max 229 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">请注意：如果在 .meg 文件的"Format"命令中指定了缺失、相同或空位符号选项，则这些选项将覆盖此处指定的设置，此处的设置保存在 MEGA 分析选项（.mao）文件中。</translation>
     </message>
     <message>
       <source>PrevOptSheet</source>
       <comment>Caption (max 12 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">上一选项页</translation>
     </message>
     <message>
       <source>Restore Defaults</source>
       <comment>Caption (max 16 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">恢复默认值</translation>
     </message>
     <message>
       <source>Save the current settings and continue</source>
       <comment>Hint (max 38 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">保存当前设置并继续</translation>
     </message>
     <message>
       <source>Save the current settings to a .mao control file</source>
       <comment>Hint (max 48 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">将当前设置保存为 .mao 控制文件</translation>
     </message>
     <message>
       <source>Sites to Include</source>
       <comment>Caption (max 16 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">包含的位点</translation>
     </message>
     <message>
       <source>Stop execution after this many hours</source>
       <comment>Hint (max 36 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">运行超过指定小时数后自动停止</translation>
     </message>
     <message>
       <source>ToolBar1</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">工具栏1</translation>
     </message>
     <message>
       <source>Transpose Data</source>
       <comment>Caption (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">转置数据</translation>
     </message>
     <message>
       <source>hours</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">小时</translation>
     </message>
   </context>
 </TS>
