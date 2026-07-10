c4f7d5fc1ff977e5b4e8c505b887b6e46dd59fae
Yaosong Li <liyaosong1@qq.com>
2026-07-10 10:32:21 +0800

    Update TAPPDEPOTFORM.ts

--- translations/zh_CN/TAPPDEPOTFORM.ts
+++ translations/zh_CN/TAPPDEPOTFORM.ts
@@ -7,27 +7,27 @@
     <message>
       <source>&amp;Cancel</source>
       <comment>Caption (max 7 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">取消(&amp;C)</translation>
     </message>
     <message>
       <source>&amp;Ok</source>
       <comment>Caption (max 3 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">确定(&amp;O)</translation>
     </message>
     <message>
       <source>HelpAction</source>
       <comment>Caption (max 10 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">帮助操作</translation>
     </message>
     <message>
       <source>MEGA AppDepot</source>
       <comment>Caption (max 13 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">MEGA 应用仓库</translation>
     </message>
     <message>
       <source>ToolBar1</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">工具栏1</translation>
     </message>
   </context>
 </TS>
