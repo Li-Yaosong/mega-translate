9f3722765e46ea00316cc2932ca4b3ea4abe2c01
Yaosong Li <liyaosong1@qq.com>
2026-07-10 11:43:22 +0800

    Update TAPPOPTIONS.ts

--- translations/zh_CN/TAPPOPTIONS.ts
+++ translations/zh_CN/TAPPOPTIONS.ts
@@ -7,62 +7,62 @@
     <message>
       <source>&amp;Cancel</source>
       <comment>Caption (max 7 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">取消(&amp;C)</translation>
     </message>
     <message>
       <source>&amp;Compute</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">计算(&amp;C)</translation>
     </message>
     <message>
       <source>&amp;Help</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">帮助(&amp;H)</translation>
     </message>
     <message>
       <source>&amp;Restore Defaults</source>
       <comment>Caption (max 17 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">恢复默认(&amp;R)</translation>
     </message>
     <message>
       <source>&amp;Save Settings...</source>
       <comment>Caption (max 17 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">保存设置(&amp;S)...</translation>
     </message>
     <message>
       <source>AppOptions</source>
       <comment>Caption (max 10 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">应用选项</translation>
     </message>
     <message>
       <source>Cancel your changes</source>
       <comment>Hint (max 19 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">取消您的更改</translation>
     </message>
     <message>
       <source>Display the help documentation</source>
       <comment>Hint (max 30 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">显示帮助文档</translation>
     </message>
     <message>
       <source>Launch the analysis with the current settings</source>
       <comment>Hint (max 45 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">使用当前设置启动分析</translation>
     </message>
     <message>
       <source>Restore all settings to their defaults</source>
       <comment>Hint (max 38 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">将所有设置恢复为默认值</translation>
     </message>
     <message>
       <source>Save current settings to an mao file</source>
       <comment>Hint (max 36 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">将当前设置保存到 MAO 文件</translation>
     </message>
     <message>
       <source>ToolBar1</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">工具栏1</translation>
     </message>
   </context>
 </TS>
