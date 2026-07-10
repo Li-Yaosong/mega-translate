3518da81311fd58fde370b327d078a71e248e93e
Yaosong Li <liyaosong1@qq.com>
2026-07-10 10:22:20 +0800

    Update TANALYSISWIZARDFORM.ts

--- translations/zh_CN/TANALYSISWIZARDFORM.ts
+++ translations/zh_CN/TANALYSISWIZARDFORM.ts
@@ -7,157 +7,157 @@
     <message>
       <source> B&amp;rowse...            </source>
       <comment>Caption (max 23 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished"> 浏览(&amp;R)...</translation>
     </message>
     <message>
       <source>&amp;Execute</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">执行(&amp;E)</translation>
     </message>
     <message>
       <source>&amp;Help</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">帮助(&amp;H)</translation>
     </message>
     <message>
       <source>&amp;Quit</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">退出(&amp;Q)</translation>
     </message>
     <message>
       <source>Analysis Wizard</source>
       <comment>Caption (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">分析向导</translation>
     </message>
     <message>
       <source>B&amp;rowse...            </source>
       <comment>Caption (max 22 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">浏览(&amp;R)...</translation>
     </message>
     <message>
       <source>CANCEL</source>
       <comment>Caption (max 6 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">取消</translation>
     </message>
     <message>
       <source>CHOOSE DATA SETTINGS</source>
       <comment>Caption (max 20 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">选择数据设置</translation>
     </message>
     <message>
       <source>DeveloperAction</source>
       <comment>Caption (max 15 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">开发者操作</translation>
     </message>
     <message>
       <source>HELP</source>
       <comment>Caption (max 4 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">帮助</translation>
     </message>
     <message>
       <source>LOAD SEQUENCE DATA</source>
       <comment>Caption (max 18 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">加载序列数据</translation>
     </message>
     <message>
       <source>LOAD TREE FILE</source>
       <comment>Caption (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">加载树文件</translation>
     </message>
     <message>
       <source>Launch the analysis</source>
       <comment>Hint (max 19 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">启动分析</translation>
     </message>
     <message>
       <source>Load the newick tree file for the phylogeny to be timed</source>
       <comment>Hint (max 55 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">加载用于定年的系统发育树 Newick 文件</translation>
     </message>
     <message>
       <source>Load the sequence data file to be used</source>
       <comment>Hint (max 38 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">加载要使用的序列数据文件</translation>
     </message>
     <message>
       <source>LocalIQTreeAction</source>
       <comment>Caption (max 17 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">本地 IQ-Tree 操作</translation>
     </message>
     <message>
       <source>MX: Timetree Wizard</source>
       <comment>Caption (max 19 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">MX: 时间树向导</translation>
     </message>
     <message>
       <source>Quit the Timetree Wizard</source>
       <comment>Hint (max 24 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">退出时间树向导</translation>
     </message>
     <message>
       <source>RESET</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">重置</translation>
     </message>
     <message>
       <source>Reset</source>
       <comment>Caption (max 5 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">重置</translation>
     </message>
     <message>
       <source>Reset this form and start over</source>
       <comment>Hint (max 30 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">重置此表单并重新开始</translation>
     </message>
     <message>
       <source>S&amp;kip This Step   </source>
       <comment>Caption (max 18 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">跳过此步(&amp;K)</translation>
     </message>
     <message>
       <source>SERVER</source>
       <comment>Caption (max 6 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">服务器</translation>
     </message>
     <message>
       <source>ServerIQTreeAction</source>
       <comment>Caption (max 18 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">服务器 IQ-Tree 操作</translation>
     </message>
     <message>
       <source>Set &amp;Options...       </source>
       <comment>Caption (max 22 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">设置选项(&amp;O)...</translation>
     </message>
     <message>
       <source>Set analysis settings for the analysis</source>
       <comment>Hint (max 38 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">设置分析参数</translation>
     </message>
     <message>
       <source>Skip this step</source>
       <comment>Hint (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">跳过此步</translation>
     </message>
     <message>
       <source>SkipTreeAction</source>
       <comment>Caption (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">跳过树操作</translation>
     </message>
     <message>
       <source>The MEGA or Fasta formatted multiple sequence alignment file which was used for inferring the phylogeny to be timed.</source>
       <comment>Hint (max 116 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">用于推断待定年系统发育树的 MEGA 或 FASTA 格式多序列比对文件。</translation>
     </message>
     <message>
       <source>The Newick formatted file that contains the phylogeny for which divergence times will be estimated.</source>
       <comment>Hint (max 99 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">包含待估计分歧时间系统发育树的 Newick 格式文件。</translation>
     </message>
     <message>
       <source>You can set preferences for substitution model to be used, distribution of evolutionary rates,  treatment of missing data, etc...</source>
       <comment>Hint (max 129 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">您可以设置替换模型、进化速率分布、缺失数据处理方式等偏好...</translation>
     </message>
   </context>
 </TS>
