c43de70029f3a6ff56b2a9ec38be9798831508e5
Yaosong Li <liyaosong1@qq.com>
2026-07-10 10:28:08 +0800

    Update TANCESTORSFRAME.ts

--- translations/zh_CN/TANCESTORSFRAME.ts
+++ translations/zh_CN/TANCESTORSFRAME.ts
@@ -7,67 +7,67 @@
     <message>
       <source>Ancestors</source>
       <comment>Caption (max 9 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">祖先</translation>
     </message>
     <message>
       <source>Export</source>
       <comment>Caption (max 6 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">导出</translation>
     </message>
     <message>
       <source>Extended Character Format</source>
       <comment>Caption (max 25 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">扩展字符格式</translation>
     </message>
     <message>
       <source>Font Size</source>
       <comment>Caption (max 9 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">字体大小</translation>
     </message>
     <message>
       <source>Font...</source>
       <comment>Caption (max 7 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">字体...</translation>
     </message>
     <message>
       <source>Hide Ambiguous</source>
       <comment>Caption (max 14 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">隐藏模糊位点</translation>
     </message>
     <message>
       <source>Jump to the next site with ancestral state change for the selected branch</source>
       <comment>Hint (max 73 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">跳转到所选分支的下一个祖先状态变化位点</translation>
     </message>
     <message>
       <source>Jump to the previous site with ancestral state change for the selected branch</source>
       <comment>Hint (max 77 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">跳转到所选分支的上一个祖先状态变化位点</translation>
     </message>
     <message>
       <source>Most Probable</source>
       <comment>Text (max 13 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">最可能状态</translation>
     </message>
     <message>
       <source>None</source>
       <comment>Caption (max 4 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">无</translation>
     </message>
     <message>
       <source>Show All</source>
       <comment>Caption (max 8 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">显示全部</translation>
     </message>
     <message>
       <source>Show Most Probable</source>
       <comment>Caption (max 18 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">显示最可能状态</translation>
     </message>
     <message>
       <source>Site # </source>
       <comment>Caption (max 7 bytes)</comment>
-      <translation type="unfinished"/>
+      <translation type="finished">位点 #</translation>
     </message>
   </context>
 </TS>
